ed01f80c7e13c391fc8d8bad6376a7445dd3fc10
Md Khalid Shahzad <khalidazam1997@gmail.com>
2025-11-05 16:51:40 +0530

    Intial Project Setup

---
+++ src/main/java/com/devon/flashsale/FlashsaleTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.devon.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashsaleTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashsaleTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devon/flashsale/FlashsaleTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devon.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
